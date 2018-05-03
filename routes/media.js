0155f14c8b9260b7e81cb07ae28f5bde1be4da5e
Jason Gallagher <grazinpalm@gmail.com>
2018-05-02 21:50:12 -0400

    Added media upload routes and updated example template

--- routes/media.js
+++ routes/media.js
@@ -3,11 +3,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var formidable = require('formidable');
+var path = require('path');
 
 /* TODO: Put this in a configuration file */
 const contentBaseDirectory = '/../public/MusicFirst/media/';
 
-
+/* File Streaming */
 router.get('/video/',function(req, res, next){
     var fileName = req.query.filename;
     var file = __dirname + contentBaseDirectory + "video/" + fileName;
@@ -71,6 +73,88 @@ router.get('/', function(req, res, next) {
         }
     });
 
+});
+
+/* File Upload */
+router.post('/video/',function(req, res, next){
+    console.log('uploading video');
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        if(err) {
+            console.log(err);
+        }
+
+        var contentType = files.videoUpload.type; // TODO: filter out invalid file types
+        var oldPath = files.videoUpload.path;
+        var newPath = path.join(__dirname, contentBaseDirectory, 'video/', files.videoUpload.name);
+
+        fs.rename(oldPath, newPath, function(err) {
+            if(err) {
+                console.log("Error uploading video, err: " + err);
+                res.write('Error uploading file');
+                res.end();
+            }
+            else {
+                res.write('success');
+                res.end();
+            }
+        });
+    });
+});
+
+router.post('/image/',function(req, res, next){
+    console.log('uploading image');
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        if(err) {
+            console.log(err);
+        }
+
+        var contentType = files.imageUpload.type; // TODO: filter out invalid file types
+        var oldPath = files.imageUpload.path;
+        var newPath = path.join(__dirname, contentBaseDirectory, 'image/', files.imageUpload.name);
+
+        fs.rename(oldPath, newPath, function(err) {
+            if(err) {
+                console.log("Error uploading video, err: " + err);
+                res.write('Error uploading file');
+                res.end();
+            }
+            else {
+                res.write('success');
+                res.end();
+            }
+        });
+    });
+});
+
+router.post('/audio/',function(req, res, next){
+    console.log('uploading audio');
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        if(err) {
+            console.log(err);
+        }
+
+        var contentType = files.audioUpload.type; // TODO: filter out invalid file types
+        var oldPath = files.audioUpload.path;
+        var newPath = path.join(__dirname, contentBaseDirectory, 'audio/', files.audioUpload.name);
+
+        fs.rename(oldPath, newPath, function(err) {
+            if(err) {
+                console.log("Error uploading video, err: " + err);
+                res.write('Error uploading file');
+                res.end();
+            }
+            else {
+                res.write('success');
+                res.end();
+            }
+        });
+    });
 });
 
 module.exports = router;
